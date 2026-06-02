3279cadb25d01ab7593dabb6a833b23c1153b90a
Meka Giridhar <mmgiridhar0@gmail.com>
2026-06-02 22:55:44 +0530

    Update static settings

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -111,10 +111,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-media_url='/media/'
-media_ROOT= BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    BASE_DIR / 'booking' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Redirect paths for login/logout
 LOGIN_REDIRECT_URL = 'home'
